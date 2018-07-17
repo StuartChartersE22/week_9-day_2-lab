59052a0cfbd78a7b0340f056542d7c0ca34a8d0a
Gregor Cox <gregorcox@gmail.com>
2018-07-17 20:07:31 +0100

    can add a new engineer

--- src/main/java/controllers/EngineerController.java
+++ src/main/java/controllers/EngineerController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import db.DBHelper;
+import models.Department;
 import models.Engineer;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class EngineerController {
 
@@ -26,5 +28,38 @@ public class EngineerController {
             model.put("engineers", engineers);
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+
+        get("/engineers/new", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            List<Department> departments = DBHelper.getAll(Department.class);
+            model.put("departments", departments);
+            model.put("template", "templates/engineers/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+        }, new VelocityTemplateEngine());
+
+        post("/engineers", (req, res) -> {
+            //get the departments id
+            int departmentId = Integer.parseInt(req.queryParams("department"));
+            //find the department by that id
+            Department department = DBHelper.find(departmentId, Department.class);
+            //get first name from request
+            String firstName = req.queryParams("first_name");
+            //get last name from request
+            String lastName = req.queryParams("last_name");
+            //get salary from request
+            int salary = Integer.parseInt(req.queryParams("salary"));
+
+            //use all these to create a new engineer
+            Engineer engineer = new Engineer(firstName, lastName, salary, department);
+            //save it using the DB Helper
+            DBHelper.save(engineer);
+            //do a redirect away from the page to the same page
+            res.redirect("/engineers");
+            return null;
+        }, new VelocityTemplateEngine());
+
+
     }
+
+
 }
