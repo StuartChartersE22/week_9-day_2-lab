787468da14d40df4e51f4166f2b6389fe05d0a17
Gregor Cox <gregorcox@gmail.com>
2018-07-17 16:38:51 +0100

    can edit and update department

--- src/main/java/controllers/DepartmentContoller.java
+++ src/main/java/controllers/DepartmentContoller.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class DepartmentContoller {
     public DepartmentContoller(){
@@ -24,5 +25,38 @@ public class DepartmentContoller {
             model.put("template", "templates/departments/index.vtl");
             return  new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
+
+        get("/departments/new", (req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/departments/create.vtl");
+            return new ModelAndView(model, "templates/layout.vtl");
+
+
+        }, new VelocityTemplateEngine());
+
+
+
+        get("/departments/:id/edit", (req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            model.put("template", "templates/departments/edit.vtl");
+            String title = req.queryParams("title");
+            int departmentId = Integer.parseInt(req.params(":id"));
+            Department department = DBHelper.find(departmentId, Department.class);
+            model.put("department", department);
+            return new ModelAndView(model, "templates/layout.vtl");
+
+            }, new VelocityTemplateEngine());
+
+        post("/departments/:id/update", (req, res) -> {
+            String title = req.queryParams("title");
+            Department department = new Department(title);
+            int departmentId = Integer.parseInt(req.params(":id"));
+            department.setId(departmentId);
+            DBHelper.save(department);
+            res.redirect("/departments");
+            return null;
+        }, new VelocityTemplateEngine());
+
+
     }
 }
